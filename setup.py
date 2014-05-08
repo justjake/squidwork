03b6555b28e6ebc5e3e5904f45cefa75816b17d7
Jake Teton-Landis <just.1.jake@gmail.com>
2014-05-08 04:08:24 -0700

    basic setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='squidwork',
+        version='0.0.1',
+        description='trivial JSON event protocol over ZeroMQ',
+        author='Jake Teton-Landis',
+        author_email='just.1.jake@gmail.com',
+        url='https://github.com/justjake/squidwork',
+        packages=['squidwork'],
+        )
