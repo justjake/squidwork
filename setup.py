da85b14fd37d43810c858ba09f8e14d89f4044d2
Jake Teton-Landis <just.1.jake@gmail.com>
2014-05-12 13:13:35 -0700

    update setup.py for requirements and submodule

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from distutils.core import setup
 
 setup(name='squidwork',
-        version='0.0.1',
+        version='0.0.2',
         description='trivial JSON event protocol over ZeroMQ',
         author='Jake Teton-Landis',
         author_email='just.1.jake@gmail.com',
         url='https://github.com/justjake/squidwork',
-        packages=['squidwork'],
+        packages=['squidwork', 'squidwork.config'],
+        install_requires=[
+          "pyzmq",
+          "pyyaml"
+          ]
         )
