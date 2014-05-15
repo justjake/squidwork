f911960385c480e7fa5584de627227688d8c2ef8
Jake Teton-Landis <just.1.jake@gmail.com>
2014-05-15 02:49:07 -0700

    applied PEP8, added import for reuse

--- squidwork/web/config.py
+++ squidwork/web/config.py
@@ -5,18 +5,23 @@ of host and port from the 'Webapp' section of a config file
 """
 
 from squidwork.config import create_argparser as base_argparser
-from squidwork.config import get_config
+from squidwork.config import get_config, get_services
+
 
 WEBAPP = 'Webapp'
 
+
 def create_argparser(**kwargs):
+    """
+    creates an argparser with all the options needed for web things
+    (namely, adds the --port flag)
+    """
     parser = base_argparser(**kwargs)
 
-    #parser.add_argument('-h', '--host', metavar='HOSTNAME OR IP',
-            #help='hostname or ip address to listen on')
     parser.add_argument('-p', '--port',
-            help='port to listen on')
+                        help='port to listen on')
     return parser
+
 
 def get_port(namespace):
     """
@@ -27,23 +32,17 @@ def get_port(namespace):
     YAML config
     """
     data = get_config(namespace.config)
-    #host = None
     port = None
 
     if WEBAPP in data:
         web_settings = data[WEBAPP]
-        #if 'host' in web_settings:
-            #host = web_settings['host']
         if 'port' in web_settings:
             port = web_settings['port']
 
     # override with command lien options
-    #host = namespace.host or host
     port = namespace.port or port
 
     # if either are None, throw a value error
-    #if host is None:
-        #raise ValueError('No hostname supplied in config, or on command line')
     if port is None:
         raise ValueError('No port supplied in config, or on command lin')
 
