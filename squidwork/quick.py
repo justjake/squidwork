4e7fa13e4d83e703bf9e1bdd380efd98e5bc0e1a
Jake Teton-Landis <just.1.jake@gmail.com>
2014-05-18 15:13:28 -0700

    autoresolve hostname -> IP for binding

--- squidwork/quick.py
+++ squidwork/quick.py
@@ -1,21 +1,37 @@
 from __future__ import print_function
 import zmq
+from urlparse import urlparse
+from socket import gethostbyname
 
 """
-the quick package makes writing trivial ZeroMQ pubsub scripts
-trivial
+the quick package makes writing trivial ZeroMQ pubsub scripts trivial.
 """
 
 context = zmq.Context.instance()
 
+
+def resolve_hostname(uri):
+    """
+    turns a hostname-based TCP uri into a IP-based URI, because ZeroMQ can't
+    bind() to a hostname for some reason
+    """
+    if not uri.startswith('tcp://'):
+        return uri
+
+    host, port = urlparse(uri).netloc.split(':')
+    return 'tcp://{ip}:{port}'.format(ip=gethostbyname(host), port=port)
+
+
 def pub(*uris):
     """
     create a publish socket, and optionally bind it.
+    resolves hostnames automatically
     """
     socket = context.socket(zmq.PUB)
     for u in uris:
-        socket.bind(u)
+        socket.bind(resolve_hostname(u))
     return socket
+
 
 def sub(*uris):
     """
